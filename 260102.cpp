a9038a0635b86d432a461d74af6024d83cc25772
danny.kang(대니) <dyhouse0517@gmail.com>
2026-01-02 09:22:17 +0900

    배열의 평균
    https://school.programmers.co.kr/learn/courses/30/lessons/120817

---
+++ 260102.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+double solution(vector<int> numbers) {
+    double answer = 0;
+    int sum = 0;
+    for (int i : numbers) {
+        sum += i;
+    }
+    answer = sum / double(numbers.size());
+    return answer;
+}
