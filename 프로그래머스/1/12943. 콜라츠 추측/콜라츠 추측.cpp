dd6982cd6967dbfa450df374b78108479c526823
danny.kang(대니) <dyhouse0517@gmail.com>
2026-01-15 08:54:40 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num) {
+    long long n = num;
+    int answer = 0;
+    if (num == 1) return 0;
+
+    while (n > 1 && answer < 500) {
+        if (n % 2 == 0) {
+            n /= 2;
+        } else {
+            n = n * 3 + 1;
+        }
+        answer++;
+    }
+    
+    if (answer >= 500 && n != 1) {
+        return -1;
+    }
+    return answer;
+}
