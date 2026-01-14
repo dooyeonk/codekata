0b58921a8e532c5d510d4f5d35f1373cd920e706
danny.kang(대니) <dyhouse0517@gmail.com>
2026-01-15 07:56:37 +0900

    바이너리 서치로 풀이 변경

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cpp
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cpp
@@ -4,17 +4,24 @@
 using namespace std;
 
 long long solution(long long n) {
-    long long answer = 0;
+    long long answer = -1;
+    if (n == 1) { return 4; }
     
-    for (long long i = 1; i <= n; ++i) {
-        if (i*i == n) {
-            answer = (i+1) * (i+1);
+    long long min = 1;
+    long long max = n;
+    
+    while (min <= max) {
+        long long mid = min + (max - min) / 2;
+        
+        if (mid == n / mid && n % mid == 0) {
+            answer = (mid + 1) * (mid + 1);
             break;
-        }
+        } else if (mid < n / mid) {
+            min = mid + 1;
+        } else {
+            max = mid - 1;
+        }        
     }
     
-    if (answer == 0) {
-        return -1;
-    }
     return answer;
 }
