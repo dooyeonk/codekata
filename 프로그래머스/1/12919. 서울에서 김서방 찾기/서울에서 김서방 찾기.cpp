234be1927689b5cfd013b4407ea656affa3f7eb3
danny.kang(대니) <dyhouse0517@gmail.com>
2026-01-15 09:11:25 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.04 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> seoul) {
+    int idx = 0;
+    
+    for (int i = 0; i < seoul.size(); ++i) {
+        if (seoul[i] == "Kim") {
+            idx = i;
+            break;
+        }
+    }
+    string answer = "김서방은 " + to_string(idx) + "에 있다";
+    return answer;
+}
